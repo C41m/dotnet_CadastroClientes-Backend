e8a498001282d0f38cc64a9d65f81980d3928bc0
C41m <caiofernandobs@gmail.com>
2024-06-06 16:36:09 -0300

    Alterações

--- CadastroPessoas/Program.cs
+++ CadastroPessoas/Program.cs
@@ -1,52 +1,20 @@
-using CadastroPessoas.DataContext;
-using CadastroPessoas.Service.CidadeService;
-using CadastroPessoas.Service.CidadesService;
-using CadastroPessoas.Service.ClienteService;
-using Microsoft.EntityFrameworkCore;
-using System.Globalization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IClienteInterface, ClienteService>();
-builder.Services.AddScoped<ICidadeInterface, CidadeService>();
-builder.Services.AddCors(options =>
+namespace CadastroPessoas
 {
-    options.AddPolicy("MyCorsPolicy", policy =>
+    public class Program
     {
-        policy.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-    });
-});
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseCors("MyCorsPolicy");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ CadastroPessoas/Startup.cs
@@ -0,0 +1,63 @@
+﻿using CadastroPessoas.DataContext;
+using CadastroPessoas.Service.CidadeService;
+using CadastroPessoas.Service.CidadesService;
+using CadastroPessoas.Service.ClienteService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CadastroPessoas
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddScoped<IClienteInterface, ClienteService>();
+            services.AddScoped<ICidadeInterface, CidadeService>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyCorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("MyCorsPolicy");
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
